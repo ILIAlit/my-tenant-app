f6fcd5d2dd1e8780af26a34035b0d58ed373592d
ILIAlit <lila96611@gmail.com>
2026-06-01 18:19:06 +0300

    add role middleware

--- routes/renter/renter.php
+++ routes/renter/renter.php
@@ -3,9 +3,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RoleMiddleware;
+use App\Enums\UserRole;
 
 
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware([RoleMiddleware::class . ':' . UserRole::ADMIN->value])->group(function () {
     Route::inertia('renter', 'admin/renter')->name('renters.get');
 });
